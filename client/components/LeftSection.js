d8011206bdc815307acea9b901e505cfe8209a4d
ragingrahul <raj17sarma@gmail.com>
2023-04-16 01:37:29 +0530

    Wallet

---
+++ client/components/LeftSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LeftSection() {
+  return (
+    <div className='w-[10%] h-[80%] bg-gradient-to-b from-[#3A3D48] to-[#28292E] flex justify-center rounded-[50px]'>
+      <h1 className='text-xl p-3 pt-[100px]'>Wallet</h1>
+    </div>
+  )
+}
+
+export default LeftSection
