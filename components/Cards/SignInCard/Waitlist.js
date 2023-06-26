e18d1e79074ba73c3fd196114c0cfbcb54787485
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-06-26 13:29:24 +0530

    font size of button:

--- components/Cards/SignInCard/Waitlist.js
+++ components/Cards/SignInCard/Waitlist.js
@@ -6,12 +6,12 @@ export const Waitlist = () => {
   return (
     <div className={`${styles.card} border-white border-[1px] w-fit flex flex-col mx-auto p-4 my-12`} >
 <span className='font-sec text-5xl text-white '>Sign up for waitlist</span> <br/>
-<p className='max-w-[50ch] text-white opacity-50 font-prm text-lg'>
+<p className='max-w-[50ch] text-white opacity-50 font-prm text-xl'>
 Bankless Wallet is a perfect platform for your web3 wallet, just plug your wallet and pay with additional features.
 </p>
 <button style={{
             background:'linear-gradient(95.51deg, #D02129 0%, #750069 100%)'
-        }} className="p-3 rounded-prm flex items-center gap-3 w-fit text-white font-prm mt-4">Sign Up <PiArrowUpRightBold/></button>
+        }} className="p-3 rounded-prm flex items-center gap-3 w-fit text-white text-xl font-prm mt-4">Sign Up <PiArrowUpRightBold/></button>
     </div>
   )
 }
