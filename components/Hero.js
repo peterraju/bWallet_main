0ac9608048aabf494819d705228728f5a35bd2d9
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-07-17 15:52:22 +0530

    early access

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import BCard from "./Cards/B-Card/B-Card";
 import {PiArrowUpRightBold} from 'react-icons/pi'
 
@@ -12,10 +13,12 @@ Minimize Risk
       Unlock the Power of Web3 with Bankless Wallet: Seamlessly Connect, Pay, and Explore with Extraordinary Features!
       </p>
       <div className="text-xl flex-col md:flex-row flex items-center justify-center mt-12 gap-6 md:gap-12">
+        <Link href="/earlyAccess" passHref={true}>
         <button style={{
             background:'linear-gradient(95.51deg, #D02129 0%, #750069 100%)'
-        }} className="px-8 py-3 md:p-3 rounded-prm flex items-center gap-3">Create Wallet <PiArrowUpRightBold/></button>
-        <button className="border border-1 px-8 py-3 md:p-3 rounded-prm flex items-center gap-3">How it Works <PiArrowUpRightBold/></button>
+        }} className="px-8 py-3 md:p-3 rounded-prm flex items-center gap-3" >Get Early Access <PiArrowUpRightBold/></button>
+        </Link>
+        {/* <button className="border border-1 px-8 py-3 md:p-3 rounded-prm flex items-center gap-3">How it Works <PiArrowUpRightBold/></button> */}
       </div>
      
     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source:'/earlyAccess',
+                destination:'https://go.banklesswallet.xyz/HW',
+                permanent:true
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
