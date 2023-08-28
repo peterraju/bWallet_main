de804b61b1fed941741f1a88cee75dab12dfb12b
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-08-28 23:38:07 +0530

    Metamask & Coinbase Wallet Check

--- components/Onboard/Step2.js
+++ components/Onboard/Step2.js
@@ -5,43 +5,25 @@ import { setGlobalState, useGlobalState, getGlobalState } from "@store";
 import { BsArrowRight } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { ethers } from "ethers";
+import { useConnect, useAccount } from "wagmi";
 
 function Step() {
-  const [mounted, setMounted] = useState(false);
-  const [web3authInstance, setWeb3authInstance] = useState(null);
-  const [address, setAddress] = useState(null);
-  const [user, setUser] = useState(null);
-  const [addr] = useGlobalState("address");
+  const { connectors, connectAsync } = useConnect();
 
-  const {
-    init,
-    login,
-    logout,
-    getPrivateKey,
-    getUserInfo,
-    checkLogin,
-    getPublicKey,
-  } = useWeb3Auth();
+  const { address } = useAccount();
 
-  const initialize = async () => {
-    const { web3authInstance } = await init();
-    setWeb3authInstance(web3authInstance);
-  };
-
-  useEffect(() => {
-    setMounted(true);
-    initialize();
-  }, []);
-
-  const connectwalletHandler = () => {
-    if (typeof window === "undefined") return;
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    if (window.Ethereum) {
-      provider.send("eth_requestAccounts", []).then(async () => {
-        await accountChangedHandler(provider.getSigner());
-      });
-    } else {
-      alert("Please install metamask");
+  const connectExternal = async (index) => {
+    try {
+      if (!address) {
+        await connectAsync({ connector: connectors[index] });
+        setGlobalState("stepCount", 3);
+        setGlobalState("loginType", "external");
+      } else {
+        setGlobalState("stepCount", 3);
+        setGlobalState("loginType", "external");
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
@@ -53,7 +35,7 @@ function Step() {
             "linear-gradient(180deg, #1E1E1E 0%, #141414 100%),linear-gradient(0deg, #EA13F2, #EA13F2)",
         }}
         className="text-sm flex items-center justify-center rounded-full py-1 shadow-xl"
-        onClick={connectwalletHandler}
+        onClick={() => connectExternal(0)}
       >
         Metamask
       </div>
@@ -63,6 +45,7 @@ function Step() {
             "linear-gradient(180deg, #1E1E1E 0%, #141414 100%),linear-gradient(0deg, #EA13F2, #EA13F2)",
         }}
         className="text-sm flex items-center justify-center rounded-full py-1"
+        onClick={() => connectExternal(1)}
       >
         Coinbase
       </div>
@@ -72,25 +55,11 @@ function Step() {
             "linear-gradient(180deg, #1E1E1E 0%, #141414 100%),linear-gradient(0deg, #EA13F2, #EA13F2)",
         }}
         className="text-sm flex items-center justify-center rounded-full py-1"
+        onClick={() => connectExternal(2)}
       >
         WalletConnect
       </div>
 
-      <div
-        style={{
-          background: "linear-gradient(90deg, #E51E2A 0%, #EA13F2 100%)",
-        }}
-        className="text-sm py-1 rounded-full gap-1 flex justify-center items-center"
-        onClick={() => {
-          if (getGlobalState("address")) {
-            setGlobalState("stepCount", 3);
-          } else {
-            alert("connect with google");
-          }
-        }}
-      >
-        Next <BsArrowRight />
-      </div>
       <div
         style={{
           background:
