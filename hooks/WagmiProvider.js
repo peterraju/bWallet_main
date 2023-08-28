8615f39c750c5e09dd057cbf208e44bcf2b2d621
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-08-28 23:37:10 +0530

    Coinbase & Metamask Setup Completed

---
+++ hooks/WagmiProvider.js
@@ -0,0 +1,40 @@
+"use client";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+
+import { goerli } from "@wagmi/core/chains";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [
+    alchemyProvider({ apiKey: "3IgRi-BWemVdwlXdm7sT6CDf-bS5ECAk" }),
+    publicProvider(),
+  ]
+);
+
+// Set up wagmi config
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "bWallet",
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+});
+
+export default function WagmiProvider({ children }) {
+  return <WagmiConfig config={config}>{children}</WagmiConfig>;
+}
