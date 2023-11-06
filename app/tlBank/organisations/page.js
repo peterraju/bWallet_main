e5a0b1b0621d4306e531288c1baf3e475ac950ec
Gautam Raj <ashragautam25@gmail.com>
2023-11-07 02:46:23 +0530

    Add Organisations page
    - Add 2 safe logos
    - Add Org list and items

--- app/tlBank/organisations/page.js
+++ app/tlBank/organisations/page.js
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import Link from "next/link";
+
+import OrganisationList from "@/components/layout/tlBank/organisation/OrganisationList";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 const OrganisationsPage = () => {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-black">
-      Organisations
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
+        <h2 className="text-4xl font-bold">Your Organisation</h2>
+        <p className="mt-4 text-2xl font-medium text-gray-600">
+          Choose a safe from your is of Organisation.
+        </p>
+
+        <OrganisationList />
+
+        <div className="mt-12 flex flex-col items-center space-y-2">
+          <p className="text-base leading-normal">
+            Don’t see your Organisation?
+          </p>
+
+          <Link
+            href="#"
+            className="text-pink flex items-center rounded-lg bg-[#FCADFF]/20 px-2 py-1 text-[#FCADFF]"
+          >
+            Join our Waitlist
+            <ChevronRightIcon className="ml-2 h-4 w-4" />
+          </Link>
+        </div>
+      </div>
     </main>
   );
 };
