76bd7bf0ef4a9812199577b4485da585fe748d21
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-11-10 21:06:47 +0530

    Added Organization Selection

--- components/layout/tlBank/organisation/OrganisationItem.js
+++ components/layout/tlBank/organisation/OrganisationItem.js
@@ -1,8 +1,22 @@
+"use client";
+import { setSafe, setSafeName } from "@/redux/slice/walletSlice";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
 
 const OrganisationItem = ({ src, name, label, pubKey }) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
   return (
-    <div className="mx-auto flex w-[46vw] max-w-2xl items-center justify-between gap-8 rounded-2xl border-2 border-gray-700 bg-gray-900/60 p-3">
+    <div
+      className="mx-auto flex w-[46vw] max-w-2xl items-center justify-between gap-8 rounded-2xl border-2 border-gray-700 bg-gray-900/60 p-3 hover:cursor-pointer"
+      onClick={() => {
+        dispatch(setSafe(pubKey));
+        dispatch(setSafeName(label));
+        router.push("/tlBank/dashboard/");
+      }}
+    >
       <div className="flex w-44 items-center gap-2">
         <div className="flex h-12 w-12 items-center justify-center rounded-full bg-white">
           <Image src={src} alt="BanklessDAO" width={24} height={24} />
@@ -11,11 +25,17 @@ const OrganisationItem = ({ src, name, label, pubKey }) => {
         <h3 className="text-base text-gray-400">{name}</h3>
       </div>
 
-      <p className="w-44 overflow-hidden text-ellipsis whitespace-nowrap text-xl">
+      <p className="w-48 overflow-hidden text-ellipsis whitespace-nowrap text-xl">
         {label}
       </p>
 
-      <p className="text-pink  text-xl">{pubKey}</p>
+      <p className="text-pink  text-xl">
+        {pubKey.toString().substring(0, 4) +
+          "..." +
+          pubKey
+            .toString()
+            .substring(pubKey.toString().length - 4, pubKey.toString().length)}
+      </p>
     </div>
   );
 };
--- components/layout/tlBank/organisation/OrganisationList.js
+++ components/layout/tlBank/organisation/OrganisationList.js
@@ -5,20 +5,38 @@ const OrganisationList = () => {
     {
       src: "/images/tlbank/org/gnosis-safe.svg",
       name: "Gnosis Safe",
-      label: "Bankless Design",
-      pubKey: "0x1234567890",
+      label: "Analytics Guild Multisig",
+      pubKey: "0x28861eeec6ab073A36Cc299ef8186978893e21FF",
     },
     {
-      src: "/images/tlbank/org/parcel-safe.svg",
-      name: "Parcel Safe",
-      label: "Spacevents Treasury Safe",
-      pubKey: "0x1234567890",
+      src: "/images/tlbank/org/gnosis-safe.svg",
+      name: "Gnosis Safe",
+      label: "AV Guild Multisig",
+      pubKey: "0xE2D721c126150BeEE3C56A1AD71A2E0E29A411F0",
     },
     {
-      src: "/logo.svg",
-      name: "Bankless Wallet",
-      label: "Xelmar Safe Treasury",
-      pubKey: "0x1234567890",
+      src: "/images/tlbank/org/gnosis-safe.svg",
+      name: "Gnosis Safe",
+      label: "Bankless Academy EOA",
+      pubKey: "0xe1887ff140bfa9d3b45d0b2077b7471124acd242",
+    },
+    {
+      src: "/images/tlbank/org/gnosis-safe.svg",
+      name: "Gnosis Safe",
+      label: "Grants Committee Multisig",
+      pubKey: "0x12BD9048b419838e25046040Dcd297aB16850280",
+    },
+    {
+      src: "/images/tlbank/org/gnosis-safe.svg",
+      name: "Gnosis Safe",
+      label: "Bankless DAO Treasury",
+      pubKey: "0x31016FFCEBFf16C20E74A8610f1650DCB5c01Df3",
+    },
+    {
+      src: "/images/tlbank/org/gnosis-safe.svg",
+      name: "Gnosis Safe",
+      label: "Dummy Safe",
+      pubKey: "0x1dBA01d7519f3CD18cBdF2B207CC3D261eEadeF3",
     },
   ];
 
