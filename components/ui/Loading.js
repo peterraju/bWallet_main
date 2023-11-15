178cb997acf449e42cbe7a215c3dab6e6e490a14
Gautam Raj <ashragautam25@gmail.com>
2023-11-15 15:50:37 +0530

    Add loader
    - Add page loading animation
    - Update export transaction button

---
+++ components/ui/Loading.js
@@ -0,0 +1,14 @@
+const Loading = () => {
+  return (
+    <svg id="triangle" width="100px" height="100px" viewBox="-3 -4 39 39">
+      <polygon
+        fill="#EFEFEF"
+        stroke="#333333"
+        stroke-width="1"
+        points="16,0 32,32 0,32"
+      ></polygon>
+    </svg>
+  );
+};
+
+export default Loading;
